5978cc3691ca2d5b34bab03ad09a0037d5da6a9d
BrujitoOz <ozemblem66@gmail.com>
2019-09-13 22:10:34 -0700

    linkedlist

--- Puppy/Puppy/LinkedList.h
+++ Puppy/Puppy/LinkedList.h
@@ -1,10 +1,37 @@
 #pragma once
 #include "Node.h"
+using namespace std;
 class LinkedList {
-	float value;
-
+	Node* Head;
+	int Size;
 public:
-
-
-
+	LinkedList() : Head(nullptr), Size(0) {}
+	~LinkedList() {
+		delete Head;
+	}
+	int	size() {
+		return Size;
+	}
+	bool empty() {
+		return Size == 0;
+	}
+	void AddFirst(int v) {
+		Node* NodeAux = new Node(v);
+		NodeAux->SetNext(Head);
+		if (NodeAux != nullptr) {
+			Head = NodeAux;
+			Size++;
+		}
+	}
+	void DeleteFirst() {
+		if (Size > 0) {
+			Node* NodeAux = Head;
+			Head = Head->GetNext();
+			delete NodeAux;
+			Size-=1;
+		}
+	}
+	int GetValueFirst() {
+		return Head->GetVal();
+	}
 };
