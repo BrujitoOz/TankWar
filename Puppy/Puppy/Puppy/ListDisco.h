85ee929618c18b97324c4b63d6daf6694ba1a7ea
BrujitoOz <ozemblem66@gmail.com>
2019-09-24 17:30:33 -0700

    trying

--- Puppy/Puppy/Puppy/ListDisco.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ListController.h"
-#include "Disco.h"
-template <class T>class ListDisco : public ListController {
-
-public:
-	ListDisco() = default;
-	~ListDisco() = default;;
-};
